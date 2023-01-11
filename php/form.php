c161958f72344398738ae21fdcc0f85b1ad545fc
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2023-01-11 11:50:39 +0100

    Update contact.html

--- php/form.php
+++ php/form.php
@@ -8,8 +8,15 @@ require_once "../PHPMailer-master/src/PHPMailer.php";
 
 require_once "vendor/autoload.php";
 
- $subject = $_POST['subject'];
- $message = $_POST['message'];
+require_once realpath(__DIR__ . '/vendor/autoload.php');
+
+// Looing for .env at the root directory
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$recaptcha = $_POST['g-recaptcha-response'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
 if (isset($_POST['submit'])) {
 // Créer une instance de classe PHPMailer
@@ -20,8 +27,8 @@ if (isset($_POST['submit'])) {
 // Connexion
     $mail->Host = "mail.infomaniak.com";
     $mail->Port = "465";
-    $mail->Username = "colin.regamey@colinrgm.ch";
-    $mail->Password = "4P33T-CxL!ah!d$";
+    $mail->Username = $_ENV["USER_NAME"];
+    $mail->Password = $_ENV["PASSWORD"];
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->CharSet = 'UTF-8';
@@ -36,9 +43,36 @@ if (isset($_POST['submit'])) {
     $mail->Body = $message;
 
     try {
-        $mail->send();
-        echo "Message has been sent successfully";
+        $res = reCaptcha($recaptcha);
+        var_dump($res);
+        if ($res['success']) {
+            $mail->send();
+            echo "Message has been sent successfully";
+            header('Location : https://colinrgm.ch/contact.html?message=success');
+        } else {
+           header('Location : https://colinrgm.ch/contact.html?message=error');
+        }
+
+
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: " . $mail->ErrorInfo;
     }
+}
+
+function reCaptcha($recaptcha)
+{
+    $secret = "6Lext84jAAAAAKL-uJPTSYeWlDoIeeHV3_xIjIJO";
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    $postvars = array("secret" => $secret, "response" => $recaptcha, "remoteip" => $ip);
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($data, true);
 }
